db2b222bf85acb52143a0ce1295aef8d06e30fae
Holger Prause <h.prause@gmx.net>
2018-04-20 06:30:35 +0200

    readme

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
 # imagemarker
 Annotation Marker Tool for Open CV
 
-Requires JDK 1.8 and maven to build
+How to build
+* Download and install latest jdk
+* Download and install maven
+* Run mvn clean install
+* In the target folder run java -jar imagemarker-executable.jar
+  or double click imagemarker-executable.jar
+
+
--- src/main/java/playground/imagemarker/Main.java
+++ src/main/java/playground/imagemarker/Main.java
@@ -13,28 +13,14 @@ import java.io.IOException;
  * Created by Holger on 07.04.2018.
  */
 public class Main extends Application {
-
-    public static Stage stage;
-
     @Override
     public void start(Stage stage) {
-
-
-
-
-
         try {
             Parent root = FXMLLoader.load(getClass().getResource("/UI.fxml"));
             Scene scene = new Scene(root);
-
             stage.setTitle("Image Marker");
             stage.setScene(scene);
-            Main.stage = stage;
             stage.show();
-
-
-
-
             root.requestFocus();
         } catch (IOException e) {
             e.printStackTrace();
@@ -42,15 +28,6 @@ public class Main extends Application {
     }
 
     public static void main(String[] args) {
-        String title = "%s      %-35s Resolution %s x %s        File %d of %35d";
-        String format = String.format(title, "Image Marker", "image_8.png", 1280, 760, 5, 10);
-        System.err.println(format);
-
-        format = String.format(title, "Image Marker", "8.png", 1280, 760, 10, 10);
-        format = String.format(title, "Image Marker", "fucking_long_name_of_file.png", 1280, 760, 10, 10);
-        System.err.println(format);
-
-
         launch(args);
     }
 }
